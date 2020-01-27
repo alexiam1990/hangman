734de2569ee7387f695861a58dfe0ec1167d4ad2
Alex <alexiam1990@hotmail.com>
2020-01-27 14:27:55 +0100

    hangman up

--- src/com/java/curso/hangman.java
+++ src/com/java/curso/hangman.java
@@ -1,7 +1,9 @@
 package com.java.curso;
 
+import java.lang.annotation.Repeatable;
 import java.util.Random;
 import java.util.Scanner;
+
 
 public class hangman {
 
@@ -21,12 +23,51 @@ public class hangman {
 		String randomWord = WORDS[randomNumber];
 		System.out.println(randomWord);
 
-		// Input word
+		//Input char to advance the game
+		System.out.println("Intente adivinar una letra:");
+		char guessLetter = ' ';
+		guessLetter = sc.next().charAt(0);
+		
+		// Input word to win the game
 		// int guessWord = ()
 		System.out.println("Intente adivinar la palabra:");
 		String guessWord = sc.nextLine();
-				
 		
+		String bufferWord = "_";
+		bufferWord = bufferWord.repeat(10);
+//		//String that will be completed
+//		String bufferWord = "_";
+//		for (int i = 0; i < randomWord.length(); i++) {
+//			bufferWord.charAt(i) = "_";
+//		}
+		String bufferPrintWord = bufferWord.replace("", " ").trim();
+		
+		System.out.println("Buffer word: " + bufferPrintWord);
+		
+//		int charPosition = guessWord.indexOf(ch);
+//		System.out.println("Posicion: " + charPosition);
+		
+		
+		// Returns index of first occurrence of character. 
+	    int firstIndex = guessWord.indexOf('A'); 
+	    System.out.println("First occurrence of char 'A'" + 
+	                       " is found at : " + firstIndex); 
+	  
+	    // Returns index of last occurrence specified character. 
+	    int lastIndex = guessWord.lastIndexOf('A'); 
+	    System.out.println("Last occurrence of char 'A' is" + 
+	                       " found at : " + lastIndex); 
+	  
+	    // Index of the first occurrence of specified char 
+	    // after the specified index if found. 
+	    int first_in = guessWord.indexOf('A', 2); 
+	    System.out.println("First occurrence of char 'A'" + 
+	                       " after index 2 : " + first_in); 
+	  
+	    int last_in = guessWord.lastIndexOf('A', 2); 
+	    System.out.println("Last occurrence of char 'A'" + 
+	                     " after index 2 is : " + last_in); 
+	  
 
 		if (guessWord.equals(randomWord)) {
 			System.out.println("Acertaste: " + randomWord);
@@ -34,7 +75,7 @@ public class hangman {
 			System.out.println("Fallaste: " + randomWord + "\nDijiste: " + guessWord);
 		}
 
-		printHangman.state_0();
+		//printHangman.state_0();
 
 	}
 }
