a43dc7627819cacda4b983d966f5c3a511d44691
Alex <alexiam1990@hotmail.com>
2020-01-31 14:27:10 +0100

    Hang Up (loop bugged - need fix)

--- src/com/java/curso/PrintHangman.java
+++ src/com/java/curso/PrintHangman.java
@@ -4,8 +4,35 @@ public class PrintHangman {
 
 	public PrintHangman() {
 		super();
+		
 	}
-
+	
+	public void printState (int tries){
+		switch (tries) {
+		case 0:
+			state_0();
+			break;
+		case 1:
+			state_1();
+			break;
+		case 2:
+			state_2();
+			break;
+		case 3:
+			state_3();
+			break;
+		case 4:
+			state_4();
+			break;
+		case 5:
+			state_5();
+			break;
+		case 6:
+			state_6();
+			break;
+		}
+	}
+	
 	public void state_0() {
 		System.out.println(
 		"  +---+  " + "\n" +
--- src/com/java/curso/hangman.java
+++ src/com/java/curso/hangman.java
@@ -15,20 +15,27 @@ public class hangman {
 
 		Boolean isWin = false;
 		Boolean isGuessLetterRepeated = true;
+		Boolean isAllGuessLetterFilled = false; // Guess word completed
+		boolean isThisGuessLetterFilled = false; // Letter inside our word. Ex: we choose A, every instance of A is
+													// filled
 
 		// Chose random word
 		int randomNumber = r.nextInt(WORDS.length);
 		String randomWord = WORDS[randomNumber];
 		System.out.println(randomWord);
 
-		char[] bufferRandomWord = new char[randomWord.length()]; // Our current guess with all our correct letter guessed
+		char[] bufferRandomWord = new char[randomWord.length()]; // Current guess with correct letters guessed
+		String printableBufferRandomWord = ""; // Printable (with spaces) buffer
+
 		for (int i = 0; i < randomWord.length(); i++) {
 			bufferRandomWord[i] = '_';
 		}
+		int posBufferRandomWord = 0;
+		int contBufferRandomWord = 0;
 		// String para sacar por pantalla
-		String bufferPrintWord = "_";
-		bufferPrintWord = bufferPrintWord.repeat(randomWord.length());
-		bufferPrintWord = bufferPrintWord.replace("", " ").trim();
+//		String bufferPrintWord = "_";
+//		bufferPrintWord = bufferPrintWord.repeat(randomWord.length());
+//		bufferPrintWord = bufferPrintWord.replace("", " ").trim();
 
 		int att = 0; // attempts
 		char guessLetter = 0; // Input letter to guess
@@ -42,30 +49,69 @@ public class hangman {
 		for (int j = 0; j < 5; j++) {
 			isGuessLetterRepeated = true;
 
+			printHangman.printState(att);
+
 			while (isGuessLetterRepeated) { // We assume letter is repeated until we input a valid (new) one
+				isAllGuessLetterFilled = false;
 				// Input char to advance the game
-				System.out.println("Intente adivinar una letra:");
+				System.out.println("Introduzca letra:");
 				guessLetter = sc.next().charAt(0);
 
 				// Check letters in previous tries
 				if (bufferGuessLetter.indexOf(guessLetter) > -1) {
 					System.out.println("Letra ya usada, posicion " + (bufferGuessLetter.indexOf(guessLetter) + 1)
 							+ " de " + bufferGuessLetter);
-				} else {
-					isGuessLetterRepeated = false;
+				} else { // Guess letter never used
+					contBufferRandomWord = 0;
+					while (!isAllGuessLetterFilled) {
+						contBufferRandomWord = 0;
+						while (!isThisGuessLetterFilled) {
+							// search if this letter exists or not
+							posBufferRandomWord = randomWord.indexOf(guessLetter, contBufferRandomWord);
+							isThisGuessLetterFilled = false;
+							contBufferRandomWord = 0;
+							if (posBufferRandomWord > 0) {
+								isAllGuessLetterFilled = false;
+								while (!isThisGuessLetterFilled) {
+									posBufferRandomWord = randomWord.indexOf(guessLetter, contBufferRandomWord);
+									if (posBufferRandomWord > 0) {
+										System.out.println(
+												"Letra " + guessLetter + " en posicion: " + posBufferRandomWord);
+										bufferRandomWord[posBufferRandomWord] = guessLetter;
+										contBufferRandomWord = posBufferRandomWord + 1;
+									} else {
+										isThisGuessLetterFilled = true;
+										isAllGuessLetterFilled = true;
+									}
+								}
+							} else {
+								att++;
+								System.out.println(
+										"Letra " + guessLetter + " no encontrada. Intentos: " + (att) + " de 6.");
+								isAllGuessLetterFilled = true;
+								// isThisGuessLetterFilled = true;
+							}
+							isGuessLetterRepeated = false;
+						}
+					}
 				}
 			}
 			bufferGuessLetter = bufferGuessLetter.concat(String.valueOf(guessLetter));
+
 			if (sc.hasNextLine())
 				sc.nextLine(); // flush scanner
-			
+
+			// We make printable our current buffer word, for easy reading
+			printableBufferRandomWord = String.copyValueOf(bufferRandomWord);
+			printableBufferRandomWord = printableBufferRandomWord.replace("", " ").trim();
+
+			System.out.println("bufferGuessLetter(" + randomWord.length() + "): " + String.valueOf(bufferGuessLetter));
 			System.out.println("bufferRandomWord(" + randomWord.length() + "): " + String.valueOf(bufferRandomWord));
-			System.out.println("bufferPrintWord(" + randomWord.length() + "): " + bufferPrintWord);
+			System.out.println("printableBufferRandomWord(" + randomWord.length() + "): " + printableBufferRandomWord);
 
 //		int charPosition = guessWord.indexOf(ch);
 //		System.out.println("Posicion: " + charPosition);
 
-			// printHangman.state_0();
 //		}
 		}
 		sc.close();
